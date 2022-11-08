a0110bf5dfd3cfe2956e08b871383456e48b18cd
Anna Slater <annakate.slater@gmail.com>
2022-11-07 19:05:17 -0600

    add navbar

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../App.css";
+
+function Header() {
+  return (
+    <div>
+      HELLO
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/pages/Accessability.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Accessability() {
+  return (
+    <div>
+      <h1>Accessability</h1>
+    </div>
+  );
+};
