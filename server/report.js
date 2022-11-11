2db2233169c575fd8e61e2a0fea7ef765f0f371f
Casazzan <nrcasazza@gmail.com>
2022-11-11 15:55:56 -0600

    add excess report

--- server/report.js
+++ server/report.js
@@ -14,23 +14,71 @@ const pool = new Pool({
     ssl: {rejectUnauthorized: false}
 });
 
-// function getServingsUsed(startDate, endDate) {
+async function getServingsUsed(startDate, endDate) {
+    const servingsMap = new Map();
+    const query = `SELECT entree_dish,entree_amt_servings,side_ingredients,side_amt_servings,appetizer_ingredients,appetizer_amt_servings FROM order_history WHERE date >= '${startDate}' AND date   <= '${endDate}'`;
+    console.log(`Performing query: ${query}`);
+    let query_res = await pool.query(query)
+        for (let i = 0; i < query_res.rowCount; i++){
+            let row = await query_res.rows[i];
+            parseParallelStrings(row.entree_dish, row.entree_amt_servings, servingsMap);
+            parseParallelStrings(row.side_ingredients, row.side_amt_servings, servingsMap);
+            parseParallelStrings(row.appetizer_ingredients, row.appetizer_amt_servings, servingsMap);
+        }
+        return servingsMap;
+}
 
-// }
+function parseParallelStrings(itemList, servingList, map) {
+    itemArr = itemList.split(",");
+    servingArr = servingList.toString().split(",").map(Number);
+    for(i = 0; i < itemArr.length; i++) {
+        if(map.has(itemArr[i]))
+            map.set(itemArr[i], map.get(itemArr[i]) + servingArr[i]);
+        else
+            map.set(itemArr[i], servingArr[i]);
+    }
+}
 
-// function getInventoryRestock() {
+async function getInventoryRestock() {
+    const query = `SELECT item_name, restock_quantity FROM inventory`;
+    console.log(`Performing query: ${query}`);
+    const items = []
+    const query_res = await pool.query(query)
+    for (let i = 0; i < query_res.rowCount; i++){
+        items.push(await query_res.rows[i]);
+    }
+    return items;
+}
 
-// }
+router.get('/excess', async (req, res) => {
+    const startDate = req.query.startDate;
+    const endDate = req.query.endDate;
+    let servingsMap = await getServingsUsed(startDate, endDate);
+    let restockList = await getInventoryRestock();
+    
+    const excessItemsList = [];
+    for(i = 0; i < restockList.length; i++) {
+        const item = restockList[i];
+        if(!servingsMap.has(item.item_name)) {
+            excessItemsList.push({"item_name": item.item_name, "restock_quantity":item.restock_quantity, "servings_used": 0});
+        }
+        else if(item.restock_quantity / 10 > servingsMap.get(item.item_name)){
+            excessItemsList.push({"item_name": item.item_name, "restock_quantity":item.restock_quantity, "servings_used": servingsMap.get(item.item_name)});
+        }
+    }
+
+    res.send(excessItemsList);
+});
 
 router.get('/restock', (req, res) => {
     const query = `SELECT item_name, minimum_amount, servings, restock_quantity FROM inventory`;
     console.log(`Performing query: ${query}`);
-    let items = []
+    let items = [];
     pool
         .query(query)
         .then(query_res => {
             for (let i = 0; i < query_res.rowCount; i++){
-                let item = query_res.rows[i];
+                const item = query_res.rows[i];
                 if(item.servings < item.minimum_amount)
                     items.push(item);
             }
